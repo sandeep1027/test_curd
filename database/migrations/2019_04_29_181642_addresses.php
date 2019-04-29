a310061157c5a93eb8229af8424a671a6a18e202
sandeep <sandeep.kumar@northhillsolutions.com>
2019-04-29 23:53:45 +0530

    add migration for addresses table

---
+++ database/migrations/2019_04_29_181642_addresses.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('address', 100);
+            $table->float('lat', 10, 6);
+            $table->float('lng', 10, 6);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
